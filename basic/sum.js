4a8db890fba42bc6f37bd1db65daf94ac1d8f633
chun <k236179@gmail.com>
2022-05-08 14:49:08 +0800

    add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+// 回傳 1 + 2 + ... + n 的結果
+function sum(n) {
+    let sum = 0;
+   for(i = 1; i <= n ; i++ )
+   sum += i;
+   return sum ;
+   
+    // return 結果
+  }
+  
+  console.log(sum(1)); // 1
+  console.log(sum(2)); // 3
+  console.log(sum(10)); // 55
+  console.log(sum(100)); // 5050
