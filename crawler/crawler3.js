b53692e92a6c644f3665c89aef85671bb788064d
chun <k236179@gmail.com>
2022-05-24 10:43:22 +0800

    更新作業

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,28 +1,21 @@
 const axios = require('axios');
-const fs = require('fs');
+const fs = require('fs/promises');
 
-fs.readFile('stock.txt','utf-8',(err,stockNO) => {
-    if (err){
-        console.error('read file error',err);
-    }else {
-        console.log('read stock no from file:', stockNO);
-        
-        (async () => {
-            try {
-                let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
-                    params:{
-                        response: "json",
-                        date: "20220301",
-                        stockNo: stockNO,
-                    },
-                });
-                console.log(response.data);
-            } catch (e) {
-                console.error(e);
-            }
-        })
-        ();
+   
+(async () => {
+    try{
+        let stockNo = await fs.readFile('stock.txt', 'utf-8');
+        console.log("read stockNo form file", stockNo);
+        let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+            params:{
+                response:'json',
+                date:'20220301',
+                stockNo:stockNo,
+            },
+        });
+        console.log(response.data);
+    }catch (e){
+        console.error(e);
     }
-});
-
+})();
 
